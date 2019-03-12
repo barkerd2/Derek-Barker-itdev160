d45b3da66cca036233785ccc5287be58bc65108c
barkerd2 <barkerd2@gmatc.matc.edu>
2019-03-12 16:17:25 -0500

    4

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,9 @@
+var salutations = 'Welcome, ';
+var name = 'derek';
+var message = 'thank you for your interest';
+
+var welcome = salutation + name + message;
+
+
+var el = document.getElementById('greeting');
+el.textContent = welcome;
