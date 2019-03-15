324bd7d5b75d8d7e42444e7235a9c987016ab3c2
barkerd2 <barkerd2@gmatc.matc.edu>
2019-03-15 15:40:57 -0500

    Completed activity 4

--- activity-4/js/scripts.js
+++ activity-4/js/scripts.js
@@ -1,12 +1,17 @@
+
 //welcome message
 var salutations = 'Welcome, ';
 var name = 'derek';
-var message = 'thank you for your interest';
+var message = ' thank you for your interest';
 var welcome = salutation + name + message;
 // get welcome message element
-getElementById('greeting');
+var el = document.getElementById('greeting');
 el.textContent = greeting;
-//price variables
+//price var
 var price = 20,
-      studentDiscount = .10,
-  		      studentPrice = price - (price * studentDiscount);
+  studentDiscount = .10,
+ studentPrice = price - (price * studentDiscount);
+var elprice = document.getElementById('price');
+elprice.textContent = price;
+var elstudentPrice = document.getElementById('studentPrice');
+elstudentPrice.textContent = studentPrice;
