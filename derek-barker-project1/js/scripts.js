f9f1b2fd36e76db2b112d75e78cfc122cc787b92
barkerd2 <barkerd2@gmatc.matc.edu>
2019-03-19 20:38:47 -0500

    project 1

--- derek-barker-project1/js/scripts.js
+++ derek-barker-project1/js/scripts.js
@@ -1,10 +1,10 @@
-
-//welcome message
+//welcome var
 var salutations = 'Welcome, ';
 var name = 'derek';
 var message = ' thank you for your interest';
-var welcome = salutation + name + message;
-// get welcome message element
+var d = new Date();
+var day = d.getDay();
+var greeting = salutations + name + message + day;
 var el = document.getElementById('greeting');
 el.textContent = greeting;
 //price var
